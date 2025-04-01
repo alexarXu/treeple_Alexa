53c30ba718bd745dd94077feae1fa3680d041521
Alexa Xu <xby04152002@163.com>
2025-04-01 00:22:27 -0400

    Add NEOFIT for feature importance testing

--- treeple/stats/__init__.py
+++ treeple/stats/__init__.py
@@ -1,6 +1,7 @@
 from .baseline import build_cv_forest, build_permutation_forest
 from .forest import build_coleman_forest, build_oob_forest
 from .permuteforest import PermutationHonestForestClassifier
+from .neofit import NeuroExplainableOptimalFIT
 
 __all__ = [
     "build_cv_forest",
@@ -8,4 +9,5 @@ __all__ = [
     "build_coleman_forest",
     "build_permutation_forest",
     "PermutationHonestForestClassifier",
+    "NeuroExplainableOptimalFIT",
 ]
